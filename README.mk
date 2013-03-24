b84dce54c4f11a261e702c1631e41a70a95bdbdb
Josh Nussbaum <joshnuss@gmail.com>
2013-03-24 03:43:23 -0400

    initial commit

---
+++ README.mk
@@ -0,0 +1,4 @@
+Test Runner LED Matrix
+----------------------
+
+Firmware and control software to display status of rspec tests on the [Adafruit Bicolor LED Square Pixel Matrix](http://www.adafruit.com/products/902)

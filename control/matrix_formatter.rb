9289fa16e87900d7781d3adf0f53d671d46cebaa
Josh Nussbaum <joshnuss@gmail.com>
2013-03-24 05:46:16 -0400

    adds rspec formatter drb client

---
+++ control/matrix_formatter.rb
@@ -0,0 +1,31 @@
+require 'rspec/core/formatters/base_formatter'
+require 'drb/drb'
+
+class MatrixFormatter < RSpec::Core::Formatters::BaseFormatter
+  def initialize(output)
+    @server = DRbObject.new_with_uri('druby://localhost:8787')
+    super
+  end
+
+  def start(example_count)
+    super
+    @server.display_tests
+  end
+
+  def stop
+    super
+    @server.display_status
+  end
+
+  def example_started(example)
+    @server.test(:start)
+  end
+
+  def example_passed(example)
+    @server.test(:pass)
+  end
+
+  def example_failed(example)
+    @server.test(:fail)
+  end
+end
